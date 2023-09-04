8a720219070f0b720092879d5419948608eb6206
WindWalk <nhygta@hotmail.com>
2023-09-04 10:02:05 +0200

    Start af opgave 2

---
+++ src/Opgave2/TCPServer.java
@@ -0,0 +1,29 @@
+package Opgave2;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class TCPServer {
+
+	public static void main(String[] args) throws Exception {
+		
+		String clientSentence;
+		String capitalizedSentence;
+		ServerSocket welcomeSocket = new ServerSocket(6789);
+		
+		while(true){
+			Socket connectionSocket = welcomeSocket.accept();
+			System.out.println("Connection from: " + connectionSocket);
+			BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
+			DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
+			clientSentence = inFromClient.readLine();
+			capitalizedSentence = clientSentence.toUpperCase() + '\n';
+			outToClient.writeBytes(capitalizedSentence);
+		}
+
+	}
+
+}
