77c1d0bb9e58f745ecc0a7dc7906816048be5f0b
WindWalk <nhygta@hotmail.com>
2023-09-04 10:53:44 +0200

    Startet opgave 2

--- src/Opgave2/TCPClient.java
+++ src/Opgave2/TCPClient.java
@@ -10,22 +10,21 @@ import java.net.Socket;
 public class TCPClient {
 
 	public static void main(String[] args) throws Exception, IOException {
-		
+
 		String sentence;
 		String modifiedSentence;
 		
-		BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
-		
 		Socket clientSocket = new Socket("localhost", 1024);
 		DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
-
+		BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
 		BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-		sentence = inFromUser.readLine();
-		outToServer.writeBytes(sentence + '\n');
-		modifiedSentence = inFromServer.readLine();
-		System.out.println("FROM SERVER: " + modifiedSentence);
 
-		clientSocket.close();
+		while (true) {
+			sentence = inFromUser.readLine();
+			outToServer.writeBytes(sentence + '\n');
+			modifiedSentence = inFromServer.readLine();
+			System.out.println(modifiedSentence);
+		}
 	}
 
 }
